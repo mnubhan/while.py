41049615460f365982cf48bb82c0004486a40917
mnubhan <76425959+mnubhan@users.noreply.github.com>
2023-03-21 19:35:59 +0000

    Create while.py

---
+++ while.py
@@ -0,0 +1,8 @@
+value_number = int(input("Please enter any number:"))
+start_number = 0
+
+while value_number != -1:
+    if value_number != -1:
+        start_number +=  value_number
+        value_number = int(input("Please enter any number:"))
+print(f"Your total number is {start_number}")
